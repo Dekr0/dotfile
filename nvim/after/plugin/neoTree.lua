1125237598b5941c1721e2ab1511ea99b9c0957c
Dekr0 <54260982+Dekr0@users.noreply.github.com>
2023-05-24 12:36:41 -0600

    Delete nvim directory

--- nvim/after/plugin/neoTree.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set('n', '<leader>nt', function() vim.cmd("Neotree") end)
-vim.keymap.set('n', '<leader>ntc', function() vim.cmd("NeoTreeClose") end)
--- nvim/init.lua
+++
@@ -1,6 +0,0 @@
-print("Initializing configuration...")
-
-require("dekr0.basic_setting")
-require("dekr0.generate_keymapper") -- Load before nnoremap
-require("dekr0.nnoremap")
-require("dekr0.packer_plugins")
