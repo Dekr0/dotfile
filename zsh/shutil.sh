ddb90da076b266cac22d643edf43e423f4c7149e
Dekr0 <dekr0.dk@protonmail.com>
2025-04-26 10:01:13 -0600

    update

--- zsh/shutil.sh
+++ zsh/shutil.sh
@@ -2,21 +2,21 @@ fd() {
     local depth="${1:-2}"
     local worker="${2:-0}"
     local dir="${3:-.}"
-    cd $(shutil --walker -walker-depth $depth --walker-worker $worker $dir)
+    cd $(shutil --walker -walker_depth $depth --walker_worker $worker $dir)
 }
 
 fdb() {
-    cd $(shutil --walker --walker-depth 3 --walker-worker 0)
+    cd $(shutil --walker --walker_depth 3 --walker_worker 0)
     zle reset-prompt
 }
 
 fdb_kitty() {
-    select=$(shutil --walker --walker-depth 3 --walker-worker 0)
+    select=$(shutil --walker --walker_depth 3 --walker_worker 0)
     kitten @ launch --type=tab --cwd $select
 }
 
 kitty_tab_fzf() {
-    shutil --kitty-fzf-tab
+    shutil --kitty_fzf_tab
 }
 
 pkg_install() {
@@ -31,7 +31,7 @@ pkg_install() {
     sudo pacman -S $pkg
     
     if [ $? -eq 0 ]; then
-        shutil --pkg-add $pkg --pkg-category $category
+        shutil --pkg_add $pkg --pkg_category $category
 
         cd $PKG_LIST_HOME
         git add .
@@ -52,7 +52,7 @@ pkg_uninstall() {
     sudo pacman -R $pkg
 
     if [ $? -eq 0 ]; then
-        shutil --pkg-rm $pkg --pkg-category $category
+        shutil --pkg_rm $pkg --pkg_category $category
 
         cd $PKG_LIST_HOME
         git add .
