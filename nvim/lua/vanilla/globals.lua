66bbc3230af088176980019d8f65e36afc80f54a
Dekr0 <dekr0.dk@protonmail.com>
2025-11-06 04:37:08 -0700

    - disable some 3rd part integration

--- nvim/lua/vanilla/globals.lua
+++ nvim/lua/vanilla/globals.lua
@@ -12,3 +12,8 @@ R = function (name)
    RELOAD(name)
    return require(name)
 end
+
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
